0b36dad77e16f6fbcfe23f3c2bceb49e715bf6ab
Reilly Featherstone <contact@reillyfeatherstone.com>
2026-04-04 17:06:22 +0100

    feat: initial commit

--- src/blocks/WhatsOnBlock/index.tsx
+++ src/blocks/WhatsOnBlock/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { Page } from '@/payload-types'
+import { MapPin } from 'lucide-react'
 
 type WhatsOnBlock = Extract<Page['layout'][0], { blockType: 'whatsOn' }>
 
@@ -9,27 +10,37 @@ export default function WhatsOnBlock({ block }: { block: WhatsOnBlock }) {
       <h2 className="text-4xl font-medium text-black text-center px-20 max-w-450 mx-auto">
         {block.title}
       </h2>
-      <div className="py-4 grid grid-cols-3">
+      <div className="py-4 grid grid-cols-3 gap-7 max-w-350 mx-auto">
         {Array.from({ length: 6 }).map((_, i) => (
-          <div key={i} className="m-2 border-gray-300 border">
-            <div className="w-full h-70 bg-gray-300 flex justify-center items-center">
-              <span className="font-medium text-gray-500 text-xl">Image</span>
-            </div>
-            <h3 className="text-2xl font-medium p-2 pt-4">Summerfolk</h3>
-            <div>
-              <p className="font-normal text-lg px-2 text-gray-900">
-                A summer of pleasure. A storm on the horizon. Rober Hastle directs Mazim Gorky's
-                razor-sharp portrait of class, privelage and denial
-              </p>
-            </div>
-            <div className="py-5">
-              <span className="font-medium p-2 text-lg text-gray-900">
-                16 Mar 2026 - 28 Apr 2026
-              </span>
-            </div>
-            <Button variant="default" size="default">
-              <span>Test</span>
-            </Button>
+          <div key={i} className="m-2 border-gray-300 border-b pb-5">
+            <a href="#">
+              <figure className="image w-full h-70 bg-gray-300 flex justify-center items-center">
+                <span className="font-medium text-gray-500 text-xl">Image</span>
+              </figure>
+
+              <div className="content px-0.5">
+                <h3 className="title text-2xl font-bold pt-4">Pride</h3>
+                <div className="description font-medium text-sm text-gray-900">
+                  Fierce, funny and full of heart, Pride is based on the award-winning film which
+                  tells the inspiring true story of two threatened communities coming together.
+                </div>
+                <div className="dates font-bold text-base text-gray-900">
+                  11 June — 12 September 2026
+                </div>
+                <div className="meta mt-2 flex justify-between items-center">
+                  <div>
+                    <Button variant="default" size="sm">
+                      <span className="text-xs text-white">Dates & Tickets</span>
+                    </Button>
+                  </div>
+
+                  <div className="font-medium flex space-x-2">
+                    <MapPin size={20} />
+                    <span className="">Olivier Theatre</span>
+                  </div>
+                </div>
+              </div>
+            </a>
           </div>
         ))}
       </div>
