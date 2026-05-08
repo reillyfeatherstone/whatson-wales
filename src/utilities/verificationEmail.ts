d22f0be9aa2be162950944deba08b607de7725ae
Reilly Featherstone <contact@reillyfeatherstone.com>
2026-05-08 21:13:29 +0100

    fixing button within verification email

--- src/utilities/verificationEmail.ts
+++ src/utilities/verificationEmail.ts
@@ -19,7 +19,7 @@ export function generateEmailHTML(args: VerificationEmailArgs) {
     <h3>Welcome to What's On Wales | Cymru</h3>
     <p>Thanks for registering for an account with us.</p>
     <p>Please verify your email address by click the below button</p>
-    <button style="font-weight:700;border:none;outline:none;background-color:#016630;color:#fff;border-radius:18px;font-size:16px;padding:18px 32px 18px">Verify your email</button>
+    <a href=${process.env.DOMAIN_URL}/account/verify?token=${args.token} style="font-weight:700;border:none;outline:none;background-color:#016630;color:#fff;border-radius:18px;font-size:16px;padding:18px 32px 18px">Verify your email</a>
     <p>Alternatively, if you're having any issues with the button. Please use the below link:</p>
     <p>${process.env.DOMAIN_URL}/account/verify?token=${args.token}</p>
   </div>`
